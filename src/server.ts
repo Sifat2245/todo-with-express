9b7a6f24e4974f69660f070ed202b2b00e4ed564
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-12-14 18:04:51 +0600

    connected with neondb and successfully stored users data

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
-import  express, { Request, Response } from "express"
-import {Pool} from 'pg'
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
+import path from "path";
 
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-const app = express()
-const port = 5000
-
+const app = express();
+const port = 5000;
 
 //db
 const pool = new Pool({
-    connectionString: "postgresql://neondb_owner:npg_GOtx7lUT8oCu@ep-autumn-tooth-adw2g31y-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-})
+  connectionString: `${process.env.PG_CONNECTION_STRING}`,
+});
 
-const initDB = async () =>{
-    await pool.query(`
+const initDB = async () => {
+  await pool.query(`
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
@@ -23,27 +25,57 @@ const initDB = async () =>{
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
     )
-        `)
-}
+        `);
 
-initDB()
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(100) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+};
 
+initDB();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('creating a server with express')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("creating a server with express");
+});
 
-app.post('/', (req: Request, res: Response) =>{
-    console.log(req.body);
+app.post("/users", async (req: Request, res: Response) => {
+  console.log(req.body);
 
+  const { name, email, age, address } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO users (name, email, age, address) VALUES ($1, $2, $3, $4) RETURNING *",
+      [name, email, age, address]
+    );
+    console.log(result.rows[0]);
     res.status(201).json({
-        success: true,
-        message: 'api working'
-    })
-})
+      message: "user stored successfully",
+      success:  true,
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    return res.status(500).json({
+      success: false,
+      message: err.message || "something went wrong",
+    });
+  }
+  res.status(201).json({
+    success: true,
+    message: "api working",
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
